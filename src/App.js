e322ece66021b3276e6f35a7f617eaee31a4ff53
binoyam <binyam.techan01@gmail.com>
2023-09-12 05:44:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-
+import Nav from "./components/Nav";
+import countryData from "./data.json";
+import ArrowDown from "./images/angle-down-solid.svg";
 function App() {
   return (
-    <div>
-     JHGJH
+    <div className="app">
+      <Nav />
+      <form>
+        <input type="text" placeholder="Search for a country..." />
+        <div className="select-options-div">
+          <div className="select-region">
+            Filter by Region
+            <button>
+              <img className="arrow" src={ArrowDown} alt="arrowdown" />
+            </button>
+          </div>
+          <ul className="options">
+            <li>Africa</li>
+            <li>America</li>
+            <li>Asia</li>
+            <li>Europe</li>
+            <li>Ocenia</li>
+          </ul>
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MoonImg from "../images/moon-regular.svg";
+
+function Nav() {
+  return (
+    <nav className="nav-bar">
+      <h1>Where in the world?</h1>
+      <div className="dark-mode-div">
+        <img className="moon-img" src={MoonImg} alt="dark-mode" />
+        <p>Dark mode</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
