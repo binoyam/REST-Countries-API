86c9f119015a6181712921a1bfc346984243b4ab
binoyam <binyam.techan01@gmail.com>
2023-09-13 04:38:17 +0300

    cant render strings/stuck

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Countries from "./components/Countries";
 import Nav from "./components/Nav";
 import Search from "./components/Search";
+import React, { useEffect, useState } from "react";
+
+const url = "https://restcountries.com/v3.1/all";
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const countryData = async () => {
+      const response = await fetch(url);
+      const countries = await response.json();
+      setCountries(countries);
+      // console.log(countries);
+    };
+    countryData();
+  }, []);
   return (
     <div className="app">
       <Nav />
-      <Search />
-     <Countries />
+      <Search countries={countries} />
+      <Countries countries={countries} />
     </div>
   );
 }
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from "react";
+function Countries(countries) {
 
-const url = "https://restcountries.com/v3.1/all";
-function Countries() {
-  const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    const countryData = async () => {
-      const response = await fetch(url);
-      const countries = await response.json();
-      setCountries(countries);
-      // console.log(countries);
-    };
-    countryData();
-  });
 
-  return <div className="countries">
-    {countries.map((country) =>(
-      <div>
-        <img src={country.flags.png} alt="" />
-      </div>
-    ))}
-
-  </div>;
+  return (
+    <>
+      {countries.map((country) => (
+        <article key={country.numericCode}>
+          <img src={country.flags.png} alt={country.name} />
+          {/* <h3>hello: {country}</h3> */}
+        </article>
+      ))}
+    </>
+  );
 }
 
 export default Countries;
