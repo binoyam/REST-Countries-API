492061830e4e12b9e7aded0811087c9828f44ff4
binoyam <binyam.techan01@gmail.com>
2023-09-16 00:47:18 +0300

    rendering fixed

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,15 +1,21 @@
-function Countries(countries) {
-
-
-
+function Countries(props) {
   return (
     <>
-      {countries.map((country) => (
-        <article key={country.numericCode}>
-          <img src={country.flags.png} alt={country.name} />
-          {/* <h3>hello: {country}</h3> */}
-        </article>
-      ))}
+      <div className="countries">
+        {props.countries.map((country) => (
+          <article key={country.numericCode}>
+            <img src={country.flags.svg} alt={country.name.common} />
+            <h3 className="name">{country.name.common}</h3>
+            <p className="population">Population: {country.population}</p>
+
+            <p className="region">Region: {country.region}</p>
+            <p className="capital">
+              {console.log(country.name)}
+              Capital: {country.capital}
+            </p>
+          </article>
+        ))}
+      </div>
     </>
   );
 }
